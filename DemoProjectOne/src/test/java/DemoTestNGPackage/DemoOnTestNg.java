36fd58b0b3cab6b5adfe6afdb930ac3a5770c073
Prakash <Prakashbrk@gmail.com>
2020-05-07 17:13:58 +0530

    Commit for first time in Git

---
+++ DemoProjectOne/src/test/java/DemoTestNGPackage/DemoOnTestNg.java
@@ -0,0 +1,30 @@
+package DemoTestNGPackage;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class DemoOnTestNg {
+	
+	@Test(priority=1,description="...Sum Method in TestNG...")
+	public void sum() {
+		System.out.println("In Maven Test Add");
+		int a=10;
+		int b=20;
+		Assert.assertEquals(30, a+b);
+	}
+	@Test(priority=2)
+	public void Sub() {
+		System.out.println("In Maven Test Sub");
+		int a=30;
+		int b=20;
+		Assert.assertEquals(30, a-b);
+	}
+	@Test(priority=3)
+	public void Mul() {
+		System.out.println("In Maven Test Mul");
+		int a=30;
+		int b=20;
+		Assert.assertEquals(600, a*b);
+	}
+
+}
